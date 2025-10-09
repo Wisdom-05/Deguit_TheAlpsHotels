08d615a050789a6d643d037c0e398451523cf81e
Wisdom <johnwisdomdeguit@gmail.com>
2025-10-09 18:25:17 +0800

    Initial commit
    Added all project assets & dependencies

---
+++ app/src/main/java/ph/edu/comteq/thealpshotels/MainActivity.kt
@@ -0,0 +1,47 @@
+package ph.edu.comteq.thealpshotels
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ph.edu.comteq.thealpshotels.ui.theme.TheAlpsHotelsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TheAlpsHotelsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TheAlpsHotelsTheme {
+        Greeting("Android")
+    }
+}
